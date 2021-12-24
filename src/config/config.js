e9a2b50fd0051e460d3ea4161f400b66081b1470
Shin Hyun <kyaryunha@naver.com>
2021-12-24 14:32:11 +0900

    asdf'

--- src/config/config.js
+++ src/config/config.js
@@ -6,24 +6,67 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const envVarsSchema = Joi.object()
     .keys({
-        NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-        PORT: Joi.number().default(3000),
-        JWT_SECRET: Joi.string().required().description('JWT secret key'),
-        JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-        JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-        JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-            .default(10)
-            .description('minutes after which reset password token expires'),
+      NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+      PORT: Joi.number().default(3000),
+      JWT_SECRET: Joi.string().required().description('JWT secret key'),
+      JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
+      JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
+      JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
+          .default(10)
+          .description('minutes after which reset password token expires'),
     })
     .unknown();
 
 const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
 if (error) {
-    throw new Error(`Config validation error: ${error.message}`);
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
-module.exports = {
-    env: envVars.NODE_ENV,
-    port: envVars.PORT,
+const envSettings = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "backend_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "backend_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "backend_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
 };
+
+
+module.exports = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  jwt: {
+    secret: envVars.JWT_SECRET,
+    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+    resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+  },
+};
+
+if (envVars.NODE_ENV) {
+  if(envVars.NODE_ENV === 'development') {
+    module.exports.development = envSettings.development;
+  }
+  else if(envVars.NODE_ENV === 'test') {
+    module.exports.test = envSettings.test;
+  }
+  else if(envVars.NODE_ENV === 'production') {
+    module.exports.production = envSettings.production;
+  }
+}
